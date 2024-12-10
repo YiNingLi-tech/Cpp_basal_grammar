9a4051fb123043bbbe3239095f8faa728088c589
YiNingLi-tech <lq_sd0818@126.com>
2024-12-10 16:58:42 +0800

    Add files via upload

---
+++ eyt2.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void yngg(int cs)
+{
+	for(int i=0;i<cs;i++)
+	{
+		cout<<"Ò»Äþgeigei";
+	}
+}
+int main()
+{
+	int a;
+	cin>>a;
+	yngg(a);
+	return 0;
+}
